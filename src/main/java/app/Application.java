77ff7d5fba01c6f7faeff9b5e30a4b1b37f58500
wqferr <wqferr@gmail.com>
2017-05-26 17:37:27 -0300

    Add text mode to main method

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,20 +1,49 @@
 package app;
 
 import app.ui.gui.GuiMainWindow;
+import app.ui.text.TextInterpreter;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.NoSuchElementException;
 
 public class Application {
     public static void main(String[] args) {
-        EventQueue.invokeLater(
-            () -> {
-                try {
-                    GuiMainWindow window = new GuiMainWindow();
-                    window.frame.setVisible(true);
-                } catch (Exception e) {
-                    System.err.printf("Error initializing window: %s", e.getMessage());
+        boolean textMode = false;
+        for (String arg : args) {
+            if ("-t".equals(arg) || "--text".equals(arg))
+                textMode = true;
+        }
+
+        if (textMode) {
+            TextInterpreter ti = new TextInterpreter();
+            String line;
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+                while ((line = in.readLine()) != null && !"exit".equals(line)) {
+                    try {
+                        ti.execute(line);
+                    } catch (IllegalArgumentException e) {
+                        System.out.printf("Error: %s\n", e.getMessage());
+                    } catch (NoSuchElementException e) {
+                        System.out.println(e.getMessage());
+                    }
                 }
+            } catch (IOException e) {
+                System.err.println("Error reading from stdin");
             }
-        );
+        } else {
+            EventQueue.invokeLater(
+                    () -> {
+                        try {
+                            GuiMainWindow window = new GuiMainWindow();
+                            window.frame.setVisible(true);
+                        } catch (Exception e) {
+                            System.err.printf("Error initializing window: %s", e.getMessage());
+                        }
+                    }
+            );
+        }
     }
 }
