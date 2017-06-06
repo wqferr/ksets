33390234323906709caa550578b7db410248b961
wqferr <wqferr@gmail.com>
2017-06-06 16:55:45 -0300

    Include images for k set behaviour in slides

--- gengraph.py
+++ gengraph.py
@@ -2,10 +2,13 @@ from matplotlib import pyplot as plt
 from scipy import genfromtxt
 import sys
 
-# plots N 1d functions
 if __name__ == '__main__':
-    color = iter(['b-', 'r-', 'g-'])
+    color = iter(['b-', 'g-', 'r-', 'k-'])
+    handles = []
     for dat in sys.argv[1:]:
         out_dat = genfromtxt(dat, delimiter=',')
-        plt.plot(out_dat, next(color))
+        g, = plt.plot(out_dat, next(color), label=dat)
+        handles.append(g)
+
+    plt.legend(handles=handles)
     plt.show()
--- src/main/java/test/GenData.java
+++ src/main/java/test/GenData.java
@@ -20,30 +20,20 @@ public class GenData {
     public static void main(String[] args) throws IOException {
         double[][] in = DataIO.read("in.csv");
         double[][] out0 = {run(new KO(), in[0])};
-        DataIO.write(out0, "out0.csv");
+        DataIO.write(out0, "k0.csv");
 
         double[][] out1 = {run(new KI(0.8, 1.1), in[0])};
-        DataIO.write(out1, "out1.csv");
+        DataIO.write(out1, "k1.csv");
 
         double[][] out2 = {run(new KII(1.2, 1.5, -1, -1.0), in[0])};
-        DataIO.write(out2, "out2.csv");
+        DataIO.write(out2, "k2.csv");
 
-        double[][] in2 = DataIO.read("in2-1.csv");
+        double[][] out3 = new KIII(2, 6, 1).run(DataIO.read("in2.csv"));
+        DataIO.write(out3, "k3.csv");
 
-        KIII k = new KIII(2, 16, 2);
-        DataIO.write(k.run(in2), "out3_default_1.csv");
+        double[][] in2 = DataIO.read("in2_cont.csv");
 
-        k = new KIII(2, 16, 2);
-        k.train(DataIO.read("train.csv"));
-        DataIO.write(k.run(in2), "out3_trained_1.csv");
-
-        in2 = DataIO.read("in2-2.csv");
-
-        k = new KIII(2, 16, 2);
-        DataIO.write(k.run(in2), "out3_default_2.csv");
-
-        k = new KIII(2, 16, 2);
-        k.train(DataIO.read("train.csv"));
-        DataIO.write(k.run(in2), "out3_trained_2.csv");
+        KIII k = new KIII(2, 6, 1);
+        DataIO.write(k.run(in2), "k3_cont.csv");
     }
 }
